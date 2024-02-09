9aa3bff08df74ad26aa331e0e43046eccde8158c
hunnbug <feldolshkolnik@gmail.com>
2024-02-09 23:36:45 +0300

    pyramids

--- progrev.sol
+++ progrev.sol
@@ -4,20 +4,40 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Progrev
 {
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    event platezh(address _address, uint _amount);
+
     struct Goi
     {
-        address userAdress;
+        address userAddress;
         uint256 balance;
     }
 
-    event platezh(address _sender, uint _balance);
+    Goi[] private gois;
+    Goi private goi;
 
-    mapping(address => uint) result;
-
-
-    function ShowLohInfo(address progretiy) public view returns(uint)
+    function SendDengi(address payable _address) public payable
     {
-        return progretiy.balance;
+        require(msg.value <= goi.balance, "you dont have enough money for this operation");
+        _address.transfer(msg.value);
+        emit platezh(_address, msg.value);
+    }
+
+    function snyatDengi() public payable 
+    {
+        require(msg.value > 0, "input value more than zero!");
+        goi.balance += msg.value;
+        emit platezh(goi.userAddress, goi.balance);
+    }
+
+    function getBalance() public view returns(uint){
+        return goi.balance;
     }
 }
+
 
